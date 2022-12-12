9ccc8f8cb5e27b0e46249db257a62a71146cca9d
k-samy <khaledsamy448@gmail.com>
2022-12-12 13:08:25 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 /**
  *main - Entry point
  *
- * Description: prints "programming is like building a multilingual puzzle"
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
@@ -16,7 +16,7 @@ int main(void)
 
 {
 
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
 }
